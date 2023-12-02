1d275f6f2f4b8fd7b0b6fc2241edbb3e00384a70
dsantos747 <d.santos74747@gmail.com>
2023-12-02 20:25:13 +0000

    Added blog and first post

--- main.js
+++ main.js
@@ -43,6 +43,13 @@ function isValidKey(key) {
   return validKeyPattern.test(key);
 }
 
+function closeMenu(event) {
+  const burger = document.getElementById('menuCheckbox');
+  if (!event.target.closest('#menu') && !event.target.closest('#menuToggle')) {
+    burger.checked = false;
+  }
+}
+
 const bubbles = document.querySelectorAll('.welcome-stack-bubble');
 const box = document.querySelector('.welcome-stack-cloud');
 
@@ -69,3 +76,5 @@ document.addEventListener('keydown', (e) => {
     // const pressedKey = e.key;
   }
 });
+
+document.addEventListener('click', closeMenu);
